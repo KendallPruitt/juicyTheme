36ec921104d7ba907376f6da3b714ea55ebeaadd
Kendall Pruitt <kendallppruitt@gmail.com>
2020-07-17 15:19:59 -0500

    Create juicy.js

---
+++ juicy.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#990000", "#008715", "#ff4800", "#004499", "#820082", "#00aed0", "#909090", "#404040", "#e50000", "#00ff27", "#ffd900", "#0075ff", "#ff00a8", "#3cebff", "#ffffff"]);
+t.prefs_.set('foreground-color', "#ffffff");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', 'rgba(0,255,40,0.5)');
